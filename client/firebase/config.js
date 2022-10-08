b802e0466935a424775cc6d84ba3fb60c22787d9
tweakdoor <60291097+tweakdoor@users.noreply.github.com>
2022-10-08 17:31:51 +0400

    firebase config changed

--- client/firebase/config.js
+++ client/firebase/config.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: "AIzaSyCT_Ts2SeoYjffveVI1G5Q2-ZxDnr4OANI",
+  authDomain: "instagrm-1384d.firebaseapp.com",
+  projectId: "instagrm-1384d",
+  storageBucket: "instagrm-1384d.appspot.com",
+  messagingSenderId: "538514451950",
+  appId: "1:538514451950:web:e72273078ebcdc6b5f32ff",
+  measurementId: "G-QXPSC26CZM"
 };
 
 const app = initializeApp(firebaseConfig);
